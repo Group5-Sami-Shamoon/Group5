5cd5920f322ae7965d9a6e8d7bc20acac7491f1c
Ofek Elgozi <ofeke209@gmail.com>
2020-12-06 15:44:26 +0200

    Fixed Contractor Menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
+#define SIZE 50
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,70 +8,93 @@ using namespace std;
 
 typedef struct 
 {
+	char ch;
 	char Firstname[20];
 	char Lastname[20];
 	float id;
 	float Daily_Salary;
-	/*
 	char City[30];
 	char Adress[30];
 	int Phone_Number;
-	*/
 } Contractor;
-void Ofek_Eilon();
+
+typedef struct
+{
+	int day;
+	int month;
+	int year;
+	float S_Hour;
+	float F_Hour;
+	char employer[20];
+	char employee[20];
+	float Hour_Salary;
+	float total;
+	float premium;
+} Job;
+
 
 int main()
 {
-	
-	return 0;
-}
-
-void Ofek_Eilon()//רק ההתחלה
-{
-    int size=0;
 	float num;
-	int i=0;
+	int i = 0, choise = 0, count = 0;
 	//קליטת המידע מהקובץ
 
 	ifstream C_In;
 	C_In.open("ContractorWorkers.txt");
-	C_In >> size;
-	Contractor* c = new Contractor[size];
-	for(i=0;i<size;i++)
-		C_In >> c[i].Firstname >> c[i].Lastname >> c[i].id;
+	Contractor c[SIZE] = { 0 };
+	for (i = 0; i < SIZE; i++)
+	{
+		C_In >> c[i].ch;
+		C_In >> c[i].Firstname;
+		C_In >> c[i].Lastname;
+		C_In >> c[i].id;
+		C_In >> c[i].Daily_Salary;
+		C_In >> c[i].City;
+		C_In >> c[i].Adress;
+		C_In >> c[i].Phone_Number;
+	}
 	C_In.close();
 
 	//החזרת המידע והשינוי שלו אם יש צורך
 
 	ofstream C_Out;
 	C_Out.open("ContractorWorkers.txt");
-	C_Out << size << endl;
-	for (i = 0; i < size; i++)
+	cout <<"               Contractor Menu               "<< endl;
+	cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+	cout << "1 - Report hours" << endl << "2 - Salary calculation" << endl << "3 - Vacation report" << endl << "4 - Work history" << endl;
+	cin >> choise;
+	switch (choise)
 	{
-		if (c[i].id==2)
+	case 1:
+		break;
+	case 2:
+
+		for (i = 0; i < SIZE; i++)
 		{
-			cout << "Please Enter ID:";
-			cin >> c[i].id;
-			C_Out << c[i].Firstname << " " << c[i].Lastname << " " << c[i].id << endl;
+			if (c[i].ch == 0)
+				break;
+			else
+			{
+				C_Out << c[i].ch << endl;
+				C_Out << c[i].Firstname << endl;
+				C_Out << c[i].Lastname << endl;
+				C_Out << c[i].id << endl;
+				C_Out << c[i].Daily_Salary << endl;
+				C_Out << c[i].City << endl;
+				C_Out << c[i].Adress << endl;
+				C_Out << c[i].Phone_Number << endl;
+			}
 		}
-		else
-			C_Out << c[i].Firstname << " " << c[i].Lastname << " " << c[i].id << endl;
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		break;
 	}
 	C_Out.close();//close file
-    //cout << "Welcome to ContractNest™." << endl << "Please enter the given to number to enter a category:" << endl;
-    //cout << "1 - Report hours" << endl << "2 - Salary calculation" << endl << "3 - Vacation report" << endl << "4 - Report hours" << endl << "5 - Work history" << endl;
-    //cin >> menu;
-    //switch (menu)
-    //{
-    //case 1:
-    //	break;
-    //case 2:
-    //	break;
-    //case 3:
-    //	break;
-    //case 4:
-    //	break;
-    //case 5:
-    //	break;
-    //}
+	return 0;
 }
+
+
